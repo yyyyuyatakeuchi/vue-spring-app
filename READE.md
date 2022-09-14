0f07410f7941e57538841afbcf95d77ae5982d55
yyyyuyatakeuchi <yuzaizhunei@gmail.com>
2022-09-14 15:15:42 +0900

    initial commit

---
+++ READE.md
@@ -0,0 +1,16 @@
+手順
+
+docker起動
+$docker-compose build
+$docker-compose up
+
+frontサーバー起動
+$docker exec -it frontend /bin/bash
+$npm run serve
+
+backendサーバー起動
+RunAndDebugボタン
+→実行環境「java」
+
+初期処理参考
+https://qiita.com/PoKoPoKoTa2ry/items/2f9b54518b8158809192
---
+++ vue_project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <input @click="HelloWorld" type="button" value="HelloWorld">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  methods: {
+      HelloWorld() {
+        this.axios.get('http://localhost:8080/hello')
+          .then((response) => {
+            alert(response.data);
+          })
+          .catch((e) => {
+            alert(e);
+          });
+      }
+    }
+}
+</script>
